46876616a8e9eb90e4fafb988624201652820e0f
Galetsy <lcahuefl@uci.edu>
2023-05-18 02:23:22 -0700

    Added comments to helper.py

--- helper.py
+++ helper.py
@@ -4,15 +4,26 @@ from HelperClass import InvertedIndex, HTMLTokenizer
 
 #Decode html string
 #Cite: https://lxml.de/elementsoup.html
-def decode_html(html_string):
-    converted = UnicodeDammit(html_string)
+def decode_html(html_string: str) -> str:
+    """decode_html takes in an HTML string as input and attempts to
+    use UniCode Dammit to detect a document's encoding and covert it to
+    Unicode
+
+    Args:
+        html_string (str): A string version of the html markup
+
+    Returns:
+        str: A converted Unicode string or original HTML string
+    """
+    #Attempts to convert the original HTML's string to a different encoding.
+    converted = UnicodeDammit(html_string) 
 
     #Return original string if unable to decode using library
     if not converted.unicode_markup:
         return html_string
     return converted.unicode_markup
 
-def cleanHtml(html_content):
+def cleanHtml(html_content: str):
     cleaner = Cleaner(page_structure=False, meta=True, comments=True,style=True, inline_style=True, processing_instructions=True, remove_unknown_tags=True)
     decoded_html = decode_html(html_content)
     clean_html = cleaner.clean_html(decoded_html)
