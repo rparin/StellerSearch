c79464bb9c3417325e9d541af2e1428da1cac8b6
Ralph <rparin@uci.edu>
2023-05-18 19:12:53 +0000

    add test func

--- ParseDev.py
+++ ParseDev.py
@@ -25,6 +25,11 @@ def storeDocNum(totalDoc:int):
 def readDocShelve(key):
     with shelve.open(f'DevShelve/DocId', 'c') as shelf:
         return shelf[key]
+
+def getDocUrl(docId:int):
+    with shelve.open(f'DevShelve/DocId', 'c') as shelf:
+        if str(docId) in shelf:
+            return shelf[str(docId)]
     
 # Opening JSON file
 def getJsonData(filePath):
@@ -34,7 +39,6 @@ def getJsonData(filePath):
     htmlContent = data['content']
     jFile.close()
     return (url, htmlContent)
-
 
 def main() -> None:
     rootDir = '/home/rparin/CS121/HW3/DEV'
@@ -46,17 +50,35 @@ def main() -> None:
                 jsonFiles.append(full_path)
 
     invIndex = InvertedIndex() #Create inverted index to hold tokens from parser
-    docId = getDocNum()
+    docId = 0
     for jFile in jsonFiles:
         docId += 1
         url, htmlContent = getJsonData(jFile)
-        # Cleans and parses HTML content into tokens then adds it to Inverted index
-        tokenizeHtml(docId=docId, invIndex=invIndex, htmlContent=htmlContent)
-        invIndex.write('DevShelve')
-        invIndex.clear()
-        writeDoc(docId, url)
-        storeDocNum(docId)
-        print(docId, url)
+
+        #Check if already parsed
+        if url == getDocUrl(docId):
+            print(f'Parsed Already -- {docId}:{url}')
+        else:
+            # Cleans and parses HTML content into tokens then adds it to Inverted index
+            tokenizeHtml(docId=docId, invIndex=invIndex, htmlContent=htmlContent)
+            # invIndex.write('DevShelve')
+            # invIndex.clear()
+            # writeDoc(docId, url)
+            # storeDocNum(docId)
+            print(docId, url)
+
+def test() -> None:
+    rootDir = '/home/rparin/CS121/HW3/DEV'
+    jsonFiles = []
+    for root, dirs, files in os.walk(rootDir):
+        for name in files:
+            if name.endswith((".json")):
+                full_path = os.path.join(root, name)
+                jsonFiles.append(full_path)
+    docId = 189
+    url, htmlContent = getJsonData(jsonFiles[docId-1])
+    invIndex = InvertedIndex() #Create inverted index to hold tokens from parser
+    tokenizeHtml(docId=docId, invIndex=invIndex, htmlContent=htmlContent)
 
 if __name__ == "__main__":
     main()
