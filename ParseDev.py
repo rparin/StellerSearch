b988d4d0b00686d8a549d7db8a5023a3cf9e4fa2
Ralph <rparin@uci.edu>
2023-05-20 01:53:27 +0000

    move invalid log

--- ParseDev.py
+++ ParseDev.py
@@ -96,10 +96,10 @@ def main() -> None:
             skip = False
             try:
                 url, htmlContent = getJsonData(jFile)
+            except:
                 docFile = open("InvalidJson.txt", "a")
                 docFile.write(f'{docId}:{url}\n')
                 docFile.close()
-            except:
                 skip = True
 
             #Check if already parsed
