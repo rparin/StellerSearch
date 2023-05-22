920e3425ddd155176600293089beb955ee6dbe40
Ralph <rparin@uci.edu>
2023-05-22 17:57:22 +0000

    update parseUrl

--- ParseUrl.py
+++ ParseUrl.py
@@ -8,18 +8,22 @@ def df_from_dict(dictObj:dict):
 def main():
     warnings.filterwarnings('ignore')
     docUrl = {}
-
+    start = 4485
     with open("docId.txt") as inFile:
         for line in inFile:
             parsed = line.split()
-            docUrl[int(parsed[0])] = parsed[1]
+            if int(parsed[0]) > start:
+                docUrl[int(parsed[0])] = parsed[1]
             
     for docId in docUrl:
         df = df_from_dict({docId:docUrl[docId]})
-        df.to_hdf(f'DevHDF5/Url.hdf5', key=str(docId))
-        print(docId)
+        df.to_hdf(f'DevHDF5/Url2.hdf5', key=str(docId))
+        print(docId, docUrl[docId])
 
 
 if __name__ == "__main__":
     main()
+    # docId = 4485
+    # df = pd.read_hdf('DevHDF5/Url.hdf5', str(docId))
+    # print(docId, df.iat[0, 0])
 # Access term cell: df.iat[0, 0]
