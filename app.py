277ebbcaa4288d45f4fb084d60a95f93b33184f3
Galetsy <lcahuefl@uci.edu>
2023-05-30 20:38:18 -0700

    pagination

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_paginate import Pagination, get_page_parameter
 import openai
 
 # Citation: https://www.geeksforgeeks.org/live-search-using-flask-and-jquery/#
@@ -28,12 +29,29 @@ def summarize_url(url):
 def home():
     urls = [
         "https://www.pokemon.com/us/pokedex/bulbasaur",
+        "https://www.pokemon.com/us/pokedex/ivysaur",
+        "https://www.pokemon.com/us/pokedex/venusaur",
         "https://www.pokemon.com/us/pokedex/charmander",
+        "https://www.pokemon.com/us/pokedex/charmeleon",
+        "https://www.pokemon.com/us/pokedex/charizard",
         "https://www.pokemon.com/us/pokedex/squirtle",
+        "https://www.pokemon.com/us/pokedex/wartortle",
+        "https://www.pokemon.com/us/pokedex/blastoise",
+        "https://www.pokemon.com/us/pokedex/caterpie",
+        "https://www.pokemon.com/us/pokedex/metapod",
+        "https://www.pokemon.com/us/pokedex/butterfree",
+        "https://www.pokemon.com/us/pokedex/weedle",
+        "https://www.pokemon.com/us/pokedex/kakuna",
     ]
-    results = [(url, summarize_url(url)) for url in urls]
-    return render_template("index.html", results=results)
 
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    offset_num_page = (page - 1) * 10
+    paginated_urls = urls[offset_num_page:offset_num_page + 10]
+    results = [(url, summarize_url(url)) for url in paginated_urls]
+
+    pagination = Pagination(page=page, total=len(urls), urls_per_page=10)
+
+    return render_template("index.html", results=results, pagination=pagination)
 
 if __name__ == "__main__":
     app.run(debug=True)
