230918a26e154130f70fc2570402468318f86c3a
Galetsy <lcahuefl@uci.edu>
2023-05-30 17:30:52 -0700

    Added functionality so that the description only appears when you click on the informational icon/emoji

--- static/script.js
+++ static/script.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $(".line").toggle(); // Toggle the visibility of the horizontal line
     $(".results-container").toggle(); // Toggle the visibility of results container
   });
+
+  $("#results-list").on("click", "#info-icon", function () {
+    ///citation: https://www.geeksforgeeks.org/jquery-parent-parents-with-examples/
+    $(this).parent().next("p").toggle();
+  });
 });
